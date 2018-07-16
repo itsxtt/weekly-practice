7f65eca099897cb79da4fc33154cb384a5d80b05
Whale Liu <daqianliu@zwdai.com>
2018-07-16 16:49:12 +0800

    Add insertion sort.

---
+++ 2018/0716/insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(nums):
+	if len(nums) > 1:
+		for i in range(1, len(nums)):
+			for j in range(i, 0, -1):
+				if nums[j] < nums[j-1]:
+					nums[j], nums[j-1] = nums[j-1], nums[j]
+				else:
+					break
+	return nums
